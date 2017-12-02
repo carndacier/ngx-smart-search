470d5326488a2f61f62c5abdb7e12e58c83c5b6d
Sacha <sacha.lachkar@gmail.com>
2017-12-02 00:03:06 +0000

    Develop (#1)
    * first working version of the smart search
    including 2 configs example
    * config update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule }           from '@angular/core';
 import { FormsModule }        from '@angular/forms';
+import { HttpClientModule }   from '@angular/common/http';
 import { BrowserModule }      from '@angular/platform-browser';
 
 import { AppComponent }       from './app.component';
@@ -13,6 +14,7 @@ import { SmartSearchModule }  from './smart-search/smart-search.module';
   imports: [
     FormsModule,
     BrowserModule,
+    HttpClientModule,
 
     SmartSearchModule
   ],
